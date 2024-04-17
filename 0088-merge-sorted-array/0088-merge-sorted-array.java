e411d7ce619b8057f9532f694bc01b9e7ac2604f
Ashish Sahoo <103375094+AashishSahoo@users.noreply.github.com>
2024-04-18 00:54:43 +0530

    Update 0088-merge-sorted-array.java

--- 0088-merge-sorted-array/0088-merge-sorted-array.java
+++ 0088-merge-sorted-array/0088-merge-sorted-array.java
@@ -1,14 +1,26 @@
 class Solution {
     public void merge(int[] nums1, int m, int[] nums2, int n) {
     //   List<Integer> l=new ArrayList<>();
-    
-    int k=0;
-    while(k<n){
-        nums1[m++]=nums2[k++];
-
+     int idx1=m-1;
+     int idx2=n-1;
+     int lst=m+n-1;
+   
+   while(idx1>=0  && idx2>=0){
+      if(nums1[idx1]>nums2[idx2]){
+        nums1[lst]=nums1[idx1];
+        idx1--;
+      }
+      else{
+        nums1[lst]=nums2[idx2];
+        idx2--;
+      }
+      lst--;
+   }
+    while(idx2>=0){
+        nums1[lst]=nums2[idx2];
+        idx2--;  
+        lst--;      
     }
-    Arrays.sort(nums1);
-
-     
-    }
+  
+  }
 }
