6f31789024ed89e2cbfbd6b20f9544d1b99b437b
Ashish Sahoo <103375094+AashishSahoo@users.noreply.github.com>
2024-04-20 12:05:15 +0530

    Update 0020-valid-parentheses.java

--- 0020-valid-parentheses/0020-valid-parentheses.java
+++ 0020-valid-parentheses/0020-valid-parentheses.java
@@ -30,3 +30,48 @@ class Solution {
         return st.isEmpty();
     }
 }
+
+
+
+
+
+
+
+
+
+
+
+class Solution {
+    public boolean isValid(String s) {
+       Stack <Character> stack=new Stack<>();
+
+
+    
+       for(char c:s.toCharArray()){
+        if(c=='(' || c=='{' || c=='['){
+            stack.push(c);
+        }
+        
+        else{
+            if(c==')'){
+                if(stack.isEmpty() || stack.pop()!='('){
+                    return false;
+                }
+            }
+              if(c=='}'){
+                if(stack.isEmpty() || stack.pop()!='{'){
+                    return false;
+                }
+            }
+              if(c==']'){
+                if(stack.isEmpty() || stack.pop()!='['){
+                    return false;
+                }
+            }
+        }
+       }
+
+
+       return stack.isEmpty();
+    }
+}
